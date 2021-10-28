8376cb031310c6356f9fd8130d22d303def4bc8b
Xilas <dahepix1@gmail.com>
2021-10-28 18:03:41 +0200

    small changes and variables

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import socket
-#import pygame
+import pygame
 from settings import *
 
-#pygame.init()
-#screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
+p1 = [0,WINDOW_HEIGHT/2 * -1]
+p2 = [WINDOW_WIDTH,WINDOW_HEIGHT/2 * -1]
+ball = BALL_POS
+
+pygame.init()
+screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+
+
 
 conn, addr = s.accept()
 print('Connection address:', addr)
