a0708a35f80646fb1401243b7f191afc4814e37b
Xilas <dahepix1@gmail.com>
2021-10-28 17:20:39 +0200

    ajdhawd

---
+++ main.py
@@ -0,0 +1,21 @@
+import socket
+import pygame
+
+from settings import *
+
+pygame.init()
+
+#screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connection address:', addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print("received data:", data)
+    conn.send(data)  # echo
+conn.close()
+
