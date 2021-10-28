484014f5accef309d4eecc578d2b956f21a792e3
MrMrThing <rasmus031299@gmail.com>
2021-10-28 18:44:49 +0200

    Yeeetus

--- client.py
+++ client.py
@@ -1,14 +1,29 @@
 import socket
 from settings import *
+import pygame
 
-Text = "Hello, World!"
-MESSAGE = bytes(Text, 'utf-8')
+p2 = [WINDOW_WIDTH,WINDOW_HEIGHT/2 * -1]
 
+pygame.init()
+screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE_C)
+connected = True
+
+
+
+
+
+
+while(connected):
+    
+    if(pygame.key.key_code == "up arrow"):
+        p2[1] = p2[1] - 1
+
+    temp = str(p2[1])
+    position = bytes(temp, 'utf-8')
+    s.send(position)
+    data = s.recv(BUFFER_SIZE_C)
+    print("received data:", data)
+
 s.close()
-
-print("received data:", data)
-
