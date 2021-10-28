8627e874adbc3d583d3c860a525791c37af78d61
Xilas <dahepix1@gmail.com>
2021-10-28 17:25:16 +0200

    added client and server scripts

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+from settings import *
+
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE_C)
+s.close()
+
+print("received data:", data)
--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 import socket
 import pygame
-
 from settings import *
 
-pygame.init()
-
+#pygame.init()
 #screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -13,9 +11,8 @@ s.listen(1)
 conn, addr = s.accept()
 print('Connection address:', addr)
 while 1:
-    data = conn.recv(BUFFER_SIZE)
+    data = conn.recv(BUFFER_SIZE_S)
     if not data: break
     print("received data:", data)
     conn.send(data)  # echo
 conn.close()
-
