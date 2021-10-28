2078fa279c21553232279c58214075882cc39fda
Xilas <dahepix1@gmail.com>
2021-10-28 18:43:22 +0200

    server changes

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ ball = BALL_POS
 
 pygame.init()
 screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
@@ -16,10 +17,29 @@ print('Connection address:', addr)
 
 running = True
 while running:
+    
     data = conn.recv(BUFFER_SIZE_S)
     message = data.decode('utf-8')
+    
     if message != "":
-        print("received data:", message)
+
+        p2 = float(message)
+        print(p2)
         conn.send(data)
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+    
 
 conn.close()
