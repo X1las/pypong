a60d587bbef6a722928c66574b4df182427b2895
X1las <dahepix1@gmail.com>
2021-11-24 17:03:47 +0100

    lesser changes

---
+++ menu.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Menu:
+    print("Hello world")
+
+    screen = pygame.display.set_mode((500, 800))
+    run = True
+    while run:
+        def draw_menu():
+            pygame.draw.rect(screen,(50,50,50),100,100,200,50)
+
+        def check_button_clicked():
+
+            pass
